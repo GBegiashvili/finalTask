2161bb2e3b866eefdb03261b65eec113bee8000d
Nicholas Braveson <giorgi.begiashvili.1@btu.edu.ge>
2021-07-10 04:31:34 +0400

    m

--- src/screens/Main/Main.jsx
+++ src/screens/Main/Main.jsx
@@ -1,8 +1,9 @@
-import React, { lazy, Suspense } from "react";
+import React, { lazy, Suspense, useEffect } from "react";
 
 import Header from "../../components/MainScreenComponents/Header/Header";
 import AddPost from "../../components/MainScreenComponents/AddPost/AddPost";
 
+import { useHistory } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 const Post = lazy(() =>
@@ -10,8 +11,17 @@ const Post = lazy(() =>
 );
 
 const Main = () => {
+  const history = useHistory();
+
   const addPostStatus = useSelector((state) => state.posts.addPostStatus);
   const postsList = useSelector((state) => state.posts.posts);
+
+  useEffect(() => {
+    if (!localStorage.getItem("token")) {
+      history.push("/");
+    }
+  }, []);
+
   return (
     <div>
       <Header />
